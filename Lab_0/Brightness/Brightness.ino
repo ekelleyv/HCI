c6c9a23be86e5adc7cd9cc3d8ec61f4b491adad1
Ed Kelley <ekelley@princeton.edu>
2013-02-11 14:39:07 -0500

    fix folder

---
+++ Lab_0/Brightness/Brightness.ino
@@ -0,0 +1,53 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int yellow = 3;
+int green = 5;
+int red = 6;   
+int index = 1;
+boolean up = false;
+int max_val = 120;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(yellow, OUTPUT);     
+  pinMode(green, OUTPUT);   
+  pinMode(red, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+  controlYellow(index);
+  controlGreen((index+40)%max_val);
+  controlRed((index+80)%max_val);
+  
+  index += 1;
+  if (index == max_val) {
+    index = 0;
+  }
+  
+  delay(10);
+}
+
+// Yellow LED control
+void controlYellow(int index) {
+ analogWrite(yellow, index);
+}
+
+// Green LED control
+void controlGreen(int index) {
+ analogWrite(green, index);
+}
+
+// Red LED control
+void controlRed(int index) {
+ analogWrite(red, index);
+}
